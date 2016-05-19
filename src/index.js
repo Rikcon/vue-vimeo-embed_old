481466fe45073bdd63acf6be5df7b95353e36588
Dmitry Odnosumov <rikcon@yandex.ru>
2016-05-19 17:02:28 +0300

    first partially working example

--- src/index.js
+++ src/index.js
@@ -1,85 +1,95 @@
 'use strict'
 
 if (!String.prototype.includes) {
-  String.prototype.includes = function() {
-    'use strict'
-    return String.prototype.indexOf.apply(this, arguments) !== -1
-  }
+    String.prototype.includes = function () {
+        'use strict'
+        return String.prototype.indexOf.apply(this, arguments) !== -1
+    }
 }
 
 const events = {
-  0: 'ready',
-  1: 'loadProgress',
-  2: 'playProgress',
-  3: 'play',
-  5: 'pause',
-  6: 'finish',
-  7: 'seek'
+    0: 'ready',
+    1: 'loadProgress',
+    2: 'playProgress',
+    3: 'play',
+    5: 'pause',
+    6: 'finish',
+    7: 'seek'
 };
 
 let pid = 0;
 
 export const VimeoPlayer = {
-  props: ['playerHeight', 'playerWidth', 'videoId'],
-  template: '<div><iframe :id="elementId" :src="src"  :width="playerWidth" height="playerHeight" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>',
-  watch: {
-    playerWidth: 'setSize',
-    playerHeight: 'setSize',
-    videoId: 'update'
-  },
-  data() {
-    pid += 1;
-    return {
-      elementId: `vimeo-player-${pid}`,
-      src: '//player.vimeo.com/video/' + this.videoId + '/?api=1&player_id=' + this.elementId
-    }
-  },
-  methods: {
-    setSize() {
-      this.player.setSize(this.playerWidth || '640', this.playerHeight || '390')
+    props: ['playerHeight', 'playerWidth', 'videoId'],
+    template: '<div><iframe :id="elementId" :src="src"  :width="playerWidth" :height="playerHeight" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>',
+    watch: {
+        videoId: 'update'
     },
-    update(videoId) {
-      this.player.api('loadVideo', videoId);
-      // this.src = '//player.vimeo.com/video/' + this.videoId + '/?api=1&player_id=' + this.elementId;
-    }
-  },
-  created() {
+    data() {
+        pid += 1;
+        return {
+            elementId: `vimeo-player-${pid}`,
+            src: '//player.vimeo.com/video/' + this.videoId + '/?api=1&player_id=' + `vimeo-player-${pid}`
+        }
+    },
+    methods: {
+        setSize() {
+            // this.player.setSize(this.playerWidth || '640', this.playerHeight || '390')
+        },
+        update(videoId) {
+            this.player.api('loadVideo', videoId);
+            console.log(this.player.api('loadVideo', videoId));
+            console.log('update fired');
+            // this.src = '//player.vimeo.com/video/' + this.videoId + '/?api=1&player_id=' + this.elementId;
+        }
+    },
+    ready() {
         this.player = $f(this.elementId);
         this.player.addEvent('ready', () => {
-        this.$emit('ready', this.player);
+            this.$emit('ready', this.player);
 
-        this.player.addEvent('pause',  () => {
-          this.$emit('pause', this.player)
+            this.player.addEvent('pause', () => {
+                this.$emit('pause', this.player)
+            });
+
+            this.player.addEvent('play', () => {
+                this.$emit('play', this.player)
+            });
+
+            this.player.addEvent('finish', () => {
+                this.$emit('finish', this.player)
+            });
+
+            this.player.addEvent('playProgress', (data) => {
+                this.$emit('playprogress', this.player, data)
+            });
+
+            this.player.addEvent('loadProgress', (data) => {
+                this.$emit('loadprogress', this.player, data)
+            });
+
+            this.player.addEvent('seek', (data) => {
+                this.$emit('seek', this.player, data)
+            });
         });
-        this.player.addEvent('finish', () => {
-          this.$emit('finish', this.player)
-        });
-        this.player.addEvent('playProgress', () => {
-          this.$emit('playProgress', this.player)
-        });
-      });
-  },
-  beforeDestroy() {
-    if (this.player !== null) {
-      this.player.destroy()
+    },
+    beforeDestroy() {
+        if (this.player !== null) {
+            this.player.destroy()
+        }
+        delete this.player
     }
-    delete this.player
-  }
 };
 
 export function install(Vue) {
-  Vue.component('vimeo', VimeoPlayer);
+    Vue.component('vimeo', VimeoPlayer);
 
-  const tag = document.createElement('script');
-  tag.src = "//f.vimeocdn.com/js/froogaloop2.min.js";
-  const firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  window.onYouTubeIframeAPIReady = function() {
-
-  }
+    const tag = document.createElement('script');
+    tag.src = "http://esells.incosm/assets/frooga.js";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
 export default {
-   VimeoPlayer, install
+    VimeoPlayer, install
 }
